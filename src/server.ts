9a0b92c6298e537e0fba215ab15b26260da87917
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-07-14 14:59:12 -0300

    Chamando a rota de cadastro de categorias

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello!" });
-})
-
-// Maneira não tão legal de debugar o código
-// app.post("/courses", (request, response) => {
-//   console.log(request.body);
-//   const { name } = request.body;
-//   return response.json({ name });
-// });
-
-// Maneira adequada de debugar
-app.post("/courses2", (request, response) => {
-  const { name } = request.body;
-  return response.json({ name });
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
