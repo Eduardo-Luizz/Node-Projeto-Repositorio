45a4b0382f8ed409e8f3e59d3abea7a9c8b56a07
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-07-14 14:27:52 -0300

    Debuggando o código

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,23 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
   return response.json({ message: "Hello!" });
 })
+
+// Maneira não tão legal de debugar o código
+// app.post("/courses", (request, response) => {
+//   console.log(request.body);
+//   const { name } = request.body;
+//   return response.json({ name });
+// });
+
+// Maneira adequada de debugar
+app.post("/courses2", (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
+});
 
 app.listen(3333, () => console.log("Server is running!"));
