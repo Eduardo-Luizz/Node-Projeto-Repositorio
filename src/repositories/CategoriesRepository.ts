569a461caa3427f484725fdaa5adf91ab15ce472
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-07-15 16:57:34 -0300

    Criando um repositorio com uma classe responsável por criar categoria

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,31 @@
+import { Category } from "../models/Category";
+
+// DTO => data transfer object
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  
+  private categories: Category[] = [];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ description, name } : ICreateCategoryDTO) : void {
+
+    const category = new Category(); 
+    Object.assign(category, { // Object.assign consegue passar um objeto para ele e consegue passar quais são os atributos
+      name,
+      description,
+      created_at: new Date()
+    });
+  
+    this.categories.push(category);
+  }
+
+}
+
+export { CategoriesRepository };
