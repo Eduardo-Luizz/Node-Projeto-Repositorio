03ca4baafdbbfe25db33a9c57c08ad86ddc5bf7b
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-07-18 09:54:05 -0300

    Tirando a responsabilidade da rota de fazer a validação - agora recebe a requisição chama o service e executa algo com a resposta

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,16 @@
 import { Router } from "express";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
+  
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: "Category already exists" })
-  }
-
-  categoriesRepository.create({ name, description});
+  createCategoryService.execute({ name, description });
 
   return response.status(201).send();
 })
