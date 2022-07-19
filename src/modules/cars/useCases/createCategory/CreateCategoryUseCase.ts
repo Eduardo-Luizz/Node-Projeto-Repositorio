f4fb50a4b1a996312202c3750761b6e6ef5cf617
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-07-19 20:43:49 -0300

    Arquitetura do peojeto para as regras de negócio

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -0,0 +1,29 @@
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+/**
+ * [X] - Definir o tipo de retorno
+ * [X] - Alterar o retorno de erro
+ * [X] - Acessar o repositório
+ */
+class CreateCategoryUseCase {
+
+  constructor(private categoriesRepository: ICategoriesRepository) {};
+
+  execute({ name, description }: IRequest): void {
+
+  const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    throw new Error("Category already exists");
+  }
+
+  this.categoriesRepository.create({ name, description});
+  }
+}
+
+export { CreateCategoryUseCase };
