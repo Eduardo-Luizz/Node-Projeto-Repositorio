f7d1cabc4ec94c167fc6a3c6ee908acaaf303e08
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-08-16 09:38:16 -0300

    Conceito de stream e Lendo os dados de upload

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,8 +1,44 @@
+import fs from "fs";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+import { parse } from 'csv-parse'
 
+interface IImportCategory {
+  name: string;
+  description: string;
+}
 
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]>{
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
+      const parseFile = parse({
+        delimiter: ','
+      });
+      stream.pipe(parseFile);
+      parseFile.on('data', async (line) => {
+        // ['name', 'description']
+        const [ name, description ] = line;
+        categories.push({ 
+          name, 
+          description
+        });
+      })
+      .on('end', () => {
+        resolve(categories);
+      })
+      .on('error', (err) => {
+        reject(err);
+      })
+    })
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+    console.log(categories);
   }
 }
 
