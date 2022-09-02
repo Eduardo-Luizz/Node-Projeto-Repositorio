191b8a064a57cc9112f4ed2ffb4826c6ab396cb0
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-09-02 14:38:53 -0300

    Refatorando com Tsyringe Injeção de dependência o use case de import category

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,15 +1,19 @@
 import fs from "fs";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 import { parse } from 'csv-parse'
+import { inject, injectable } from "tsyringe";
 
 interface IImportCategory {
   name: string;
   description: string;
 }
 
+@injectable()
 class ImportCategoryUseCase {
 
-  constructor(private categoriesRepository: ICategoriesRepository) {}
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository) {}
 
   loadCategories(file: Express.Multer.File): Promise<IImportCategory[]>{
     return new Promise((resolve, reject) => {
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,14 +2,15 @@ import { Router } from "express";
 import multer from "multer";
 
 import { CreateCategoryController } from "../modules/cars/useCases/createCategory/createCategoryController";
+import { ImportCategoryController } from "../modules/cars/useCases/importCategory/ImportCategoryController";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
-import { importCategoryController } from "../modules/cars/useCases/importCategory";
 
 const categoriesRoutes = Router();
 
 const upload = multer({ dest: "./tmp" })
 
 const createCategoryController = new CreateCategoryController();
+const importCategoryController = new ImportCategoryController();
 
 categoriesRoutes.post("/", createCategoryController.handle);
 
@@ -17,8 +18,6 @@ categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
 });
 
-categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-  return importCategoryController.handle(request, response);
-})
+categoriesRoutes.post("/import", upload.single("file"), importCategoryController.handle);
 
 export { categoriesRoutes };
