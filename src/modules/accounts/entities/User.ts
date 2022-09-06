7ce0127c2b1fb65eefe9c3602d264f14fce25021
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-09-06 13:49:27 -0300

    Criando Entity de Users

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,38 @@
+import { v4 as uuidV4 } from "uuid";
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+
+@Entity("users")
+class Users {
+
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  driver_license: string;
+
+  @Column()
+  idAdmin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if(!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { Users };
