403a791c1133dabac320d42ca9d5a39fd4ce7731
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-09-16 10:03:04 -0300

    Realizando o upload de avatar

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,6 +1,18 @@
 import multer from "multer";
 import { resolve } from "path";
+import crypto from "crypto";
 
 export default {
-  upload(folder: string)
+  upload(folder: string) {
+    return {
+      storage: multer.diskStorage({
+        destination: resolve(__dirname, "..", "..", folder), // Volta para a raiz do projeto e pasta tmp
+        filename: (request, file, callback) => {
+          const fileHash = crypto.randomBytes(16).toString("hex"); //Cria um hash
+          const filename = `${ fileHash }-${ file.originalname }`  //Concatena o hash criado com o nome original
+          return callback(null, filename);
+        }
+      })
+    }
+  }
 }
