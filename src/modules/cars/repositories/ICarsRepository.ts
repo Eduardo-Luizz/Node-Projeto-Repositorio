9ed00fe922153078b5e88d78905b423dd26b03ec
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-09-30 11:23:32 -0300

    Adicionando o metodo na interface de cars

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -4,6 +4,7 @@ import { Car } from "../infra/typeorm/entities/Cars";
 interface ICarsRepository {
   create(data: ICreateCarDTO): Promise<Car>;
   findByLicensePlate(licensePlate: string): Promise<Car>;
+  findAvailable(brand?: string, category_id?: string, name?: string): Promise<Car[]>;
 }
 
 export { ICarsRepository };
