fa69a555bed568a33ac22c0253f0eabed35e153f
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-10-07 15:07:59 -0300

    Continuação dos CreateSpecificationUseCase

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -12,7 +12,7 @@ class CarsRepository implements ICarsRepository {
     this.repository = getRepository(Car);
   }
 
-  async create({ brand, category_id, daily_rate, description, fine_amount, name, license_plate }: ICreateCarDTO): Promise<Car> {
+  async create({ brand, category_id, daily_rate, description, fine_amount, name, license_plate, specifications }: ICreateCarDTO): Promise<Car> {
     const car = this.repository.create({
       brand,
       category_id,
@@ -20,7 +20,8 @@ class CarsRepository implements ICarsRepository {
       description,
       fine_amount,
       name,
-      license_plate
+      license_plate,
+      specifications
     })
     await this.repository.save(car)
     return car;
@@ -51,6 +52,11 @@ class CarsRepository implements ICarsRepository {
     const cars = await carsQuery.getMany();
 
     return cars;
+  }
+
+  async findById(id: string): Promise<Car> {
+    const car = await this.repository.findOne(id);
+    return car;
   }
 }
 
--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -1,4 +1,5 @@
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
@@ -8,15 +9,26 @@ interface IRequest {
 
 class CreateCarSpecificationUseCase {
 
-  constructor(private carsRepository: ICarsRepository) {}
+  constructor(
+    private carsRepository: ICarsRepository,
+    private specificationsRepository: ISpecificationsRepository
+  ) { }
 
   async execute({ car_id, specifications_id }: IRequest): Promise<void> {
 
     const carExists = await this.carsRepository.findById(car_id)
 
-    if(!carExists) {
+    if (!carExists) {
       throw new AppError("Car doesn't exist")
     }
+
+    const specifications = await this.specificationsRepository.findByIds(specifications_id);
+
+    carExists.specifications = specifications;
+
+    await this.carsRepository.create(carExists);
+    
+    console.log(carExists);
   }
 }
 
