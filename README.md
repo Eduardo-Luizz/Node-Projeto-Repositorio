d14d27e780069fcb453d371ab082c5ee6aa50ecd
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-10-07 14:38:28 -0300

    Caso de uso do cadastro de especificação para carro

--- README.md
+++ README.md
@@ -26,8 +26,6 @@
 
 **RF** => Requisitos Funcionais
   Deve ser possível cadastrar uma especificação para um carro.
-  Deve ser possível listar todas as especificações.
-  Deve ser possível listar todos os carros.
 
 **RN** => Regra de Negócio
   Não deve ser possível cadastar uma especificação para um carro não cadastrado.
--- src/modules/cars/infra/typeorm/entities/Cars.ts
+++ src/modules/cars/infra/typeorm/entities/Cars.ts
@@ -1,6 +1,7 @@
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, PrimaryColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 import { Category } from "./Category";
+import { Specification } from "./Specification";
 
 @Entity("cars")
 class Car {
@@ -10,22 +11,22 @@ class Car {
 
   @Column()
   name: string;
-  
+
   @Column()
   description: string;
-  
+
   @Column()
   daily_rate: number;
-  
+
   @Column()
   available: boolean;
 
   @Column()
   license_plate: string;
-  
+
   @Column()
   fine_amount: number;
-  
+
   @Column()
   brand: string;
 
@@ -35,6 +36,15 @@ class Car {
 
   @Column()
   category_id: string;
+
+  // Tabela de relacionamento é Many to Many
+  @ManyToMany(() => Specification)
+  @JoinTable({
+    name: "specifications_cars",
+    joinColumns: [{ name: "car_id" }], // nome da coluna dentro da tavela de relacionamento que referencia a tabela que estamos
+    inverseJoinColumns: [{ name: "specification_id" }] // nome da outra coluna que referencia a tabela passada no many to many
+  })
+  specifications: Specification[];
 
   @CreateDateColumn()
   created_at: Date;
