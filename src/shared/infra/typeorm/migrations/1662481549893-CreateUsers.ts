c9e2285ad31119b72cfc9cd04d4deeabba816f0f
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-10-11 14:26:24 -0300

    Criando migrations do aluguel

--- src/shared/infra/typeorm/migrations/1662481549893-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1662481549893-CreateUsers.ts
@@ -9,7 +9,8 @@ export class CreateUsers1662481549893 implements MigrationInterface {
                 columns: [
                     {
                         name: "id",
-                        type: "uuid"
+                        type: "uuid",
+                        isPrimary: true
                     },
                     {
                         name: "name",
